b6a5feadb45794bb0d719cc837cd5d5cb206e418
Ventrtom <127134999+Ventrtom@users.noreply.github.com>
2025-07-29 11:57:07 +0200

    Refactor background jobs and add PII redaction

--- agent/__init__.py
+++ agent/__init__.py
@@ -8,10 +8,12 @@ handle_query = core.handle_query
 handle_query_stream = core.handle_query_stream
 agent_workflow = core.agent_workflow
 ResearchResponse = core.ResearchResponse
+start_background_jobs = core.start_background_jobs
  
 __all__ = [
      "handle_query",
      "handle_query_stream",
-     "agent_workflow",
-     "ResearchResponse",
+    "agent_workflow",
+    "ResearchResponse",
+    "start_background_jobs",
     ]
